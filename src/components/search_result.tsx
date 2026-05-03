1ae8e9ae9b841ee001e9ef0c202d73750e6adb8b
Sean Dohring <seandohring@gmail.com>
2026-05-03 18:41:37 -0400

    buttons

--- src/components/search_result.tsx
+++ src/components/search_result.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { OpenInBrowser } from 'google-material-icons/outlined';
+import { OpenInBrowser, AddBox } from 'google-material-icons/outlined';
 
 import { ProjectHit } from "../api/types";
+import { BUTTON_STYLE } from "../style";
 
 const MAX_VERSIONS_TO_SHOW = 5;
 
@@ -17,70 +18,92 @@ export class SearchResult extends React.Component<Props> {
       versions = versions.slice(0, MAX_VERSIONS_TO_SHOW).concat([`+ ${project.versions.length - MAX_VERSIONS_TO_SHOW} more`]);
     }
     return (
-      <div className="search-result"
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          padding: '0.5rem',
-        }}>
-        <img src={project.icon_url ?? ''}
-          alt={`${project.title} icon`}
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+        gap: '0.5rem',
+      }}>
+        <div className="search-result"
           style={{
-            width: '4rem',
-            height: '4rem',
-            alignSelf: 'center',
-            marginRight: '1rem',
+            display: 'flex',
+            flexDirection: 'row',
+            padding: '0.5rem',
+            flex: 1,
+            backgroundColor: 'rgba(0, 0, 0, 0.1)',
             borderRadius: '1rem',
-          }}
-        />
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column',
-          flex: 1,
-        }}>
-          <span style={{
-            fontSize: '1rem',
-            fontWeight: 'bold',
-          }}>{project.title}</span>
-          <span style={{
-            fontSize: '0.9rem',
-          }}>{project.description}</span>
-          <div>
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+          }}>
+          <img src={project.icon_url ?? ''}
+            alt={`${project.title} icon`}
+            style={{
+              width: '4rem',
+              height: '4rem',
+              alignSelf: 'center',
+              marginRight: '1rem',
+              borderRadius: '1rem',
+            }}
+          />
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            flex: 1,
+          }}>
             <span style={{
-              fontSize: '0.7rem',
-            }}>Versions: {versions.map((version, index) => {
-              return (
-                <span key={index}
-                  style={{
-                    border: '1px solid rgba(0, 0, 0, 0.3)',
-                    borderRadius: '10rem',
-                    padding: '0.1rem 0.3rem',
-                    marginRight: '0.3rem',
-                  }}
-                >{version}</span>
-              );
-            })}</span>
+              fontSize: '1rem',
+              fontWeight: 'bold',
+            }}>{project.title}</span>
+            <span style={{
+              fontSize: '0.9rem',
+            }}>{project.description}</span>
+            <div>
+              <span style={{
+                fontSize: '0.7rem',
+              }}>Versions: {versions.map((version, index) => {
+                return (
+                  <span key={index}
+                    style={{
+                      border: '1px solid rgba(0, 0, 0, 0.3)',
+                      borderRadius: '10rem',
+                      padding: '0.1rem 0.3rem',
+                      marginRight: '0.3rem',
+                    }}
+                  >{version}</span>
+                );
+              })}</span>
+            </div>
           </div>
         </div>
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-        }}>
-          <a href={`https://modrinth.com/${project.project_type}/${project.slug}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            title={`View on Modrinth`}
-            style={{
-              color: 'inherit',
-              border: '1px solid rgba(0, 0, 0, 0.3)',
-              borderRadius: '10rem',
-              lineHeight: '1rem',
-              padding: '0.25rem',
-            }}>
-            <OpenInBrowser />
-          </a>
+
+        <div className="button"
+          style={{
+            ...BUTTON_STYLE,
+            '--data-hover-background': 'rgba(75, 138, 186, 0.8)',
+            '--data-background': 'rgba(75, 138, 186, 0.3)',
+            '--data-color': '#000',
+            '--data-hover-color': '#fff',
+            cursor: 'pointer',
+          } as React.CSSProperties}
+        >
+          <AddBox size={32} />
+          <span>Add to<br />Pack</span>
         </div>
+
+        <a href={`https://modrinth.com/${project.project_type}/${project.slug}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          title={`View on Modrinth`}
+          className="button"
+          style={{
+            ...BUTTON_STYLE,
+            '--data-hover-background': 'rgba(78, 173, 101, 0.8)',
+            '--data-background': 'rgba(78, 173, 101, 0.3)',
+            '--data-color': '#000',
+            '--data-hover-color': '#fff',
+          } as React.CSSProperties}>
+          <OpenInBrowser size={32} />
+          <span>View on<br />Modrinth</span>
+        </a>
       </div>
     );
   }
---
+++ src/style.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const BUTTON_STYLE: React.CSSProperties = {
+  borderRadius: '1rem',
+  lineHeight: '1rem',
+  padding: '0.5rem',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  textAlign: 'center',
+  justifyContent: 'center',
+  textDecoration: 'none',
+  fontSize: '0.7rem',
+  minWidth: '3rem',
+};
